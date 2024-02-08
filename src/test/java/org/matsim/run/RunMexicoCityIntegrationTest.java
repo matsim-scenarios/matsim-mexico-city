bd9c264b1c6adda79a301b5d9f9a76bde71fbe5c
simei94 <meinhardt@vsp.tu-berlin.de>
2024-02-08 11:35:14 -0600

    prepare integration test

---
+++ src/test/java/org/matsim/run/RunMexicoCityIntegrationTest.java
@@ -0,0 +1,43 @@
+package org.matsim.run;
+
+import org.junit.Test;
+import org.matsim.application.MATSimApplication;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.simwrapper.SimWrapperConfigGroup;
+
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RunMexicoCityIntegrationTest {
+
+	@Test
+	public void runPoint1PctIntegrationTest() {
+
+		double sampleSize = 0.001;
+
+		Path outputPath = Path.of("output/it-" + sampleSize * 100 + "pct");
+
+		Config config = ConfigUtils.loadConfig("input/v1.0/mexico-city-v1.0-1pct.input.config.xml");
+
+		config.global().setNumberOfThreads(1);
+		config.qsim().setNumberOfThreads(1);
+		config.qsim().setFlowCapFactor(sampleSize);
+		config.qsim().setStorageCapFactor(sampleSize);
+		config.controler().setLastIteration(1);
+		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+		config.controler().setOutputDirectory(outputPath.toString());
+//		TODO: create this plans file
+		config.plans().setInputFile("mexico-city-initial-" + sampleSize * 100 + "pct.plans.xml.gz");
+
+		ConfigUtils.addOrGetModule(config, SimWrapperConfigGroup.class).defaultDashboards = SimWrapperConfigGroup.Mode.disabled;
+
+		assert MATSimApplication.execute(RunMexicoCityScenario.class) == 0 : "Must return non error code";
+
+		assertThat(outputPath)
+			.exists()
+			.isNotEmptyDirectory();
+	}
+}
