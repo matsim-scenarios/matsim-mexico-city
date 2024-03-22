198a99a04424c42f7592e3b129c4d4ebd0dac5e4
simei94 <meinhardt@vsp.tu-berlin.de>
2024-03-21 18:36:54 -0600

    make income dependent scoring obligatory

--- src/test/java/org/matsim/run/RunMexicoCityIntegrationTest.java
+++ src/test/java/org/matsim/run/RunMexicoCityIntegrationTest.java
@@ -36,7 +36,8 @@ public class RunMexicoCityIntegrationTest {
 
 		ConfigUtils.addOrGetModule(config, SimWrapperConfigGroup.class).defaultDashboards = SimWrapperConfigGroup.Mode.disabled;
 
-		assert MATSimApplication.execute(RunMexicoCityScenario.class, config, "run", "--1pct", "--bikes-on-network") == 0 : "Must return non error code";
+		assert MATSimApplication.execute(RunMexicoCityScenario.class, config, "run", "--1pct", "--bikes-on-network",
+			"--income-area", "input/v1.0/nivel_amai/nivel_amai.shp") == 0 : "Must return non error code";
 
 		assertThat(outputPath)
 			.exists()
