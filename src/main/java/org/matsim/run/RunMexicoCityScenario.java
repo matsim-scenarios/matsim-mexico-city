2e8dbd7c9682cc85e1d7074d2f585b454c6208c4
simei94 <meinhardt@vsp.tu-berlin.de>
2024-05-16 18:02:16 +0200

    change everything to v1.x

--- src/main/java/org/matsim/run/RunMexicoCityScenario.java
+++ src/main/java/org/matsim/run/RunMexicoCityScenario.java
@@ -89,7 +89,7 @@ public class RunMexicoCityScenario extends MATSimApplication {
 	@CommandLine.Mixin
 	private final SampleOptions sample = new SampleOptions(1);
 
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.x";
 
 	public RunMexicoCityScenario(@Nullable Config config) {
 		super(config);
